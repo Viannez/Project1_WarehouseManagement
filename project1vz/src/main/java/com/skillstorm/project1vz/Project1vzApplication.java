6e2e0d25264dbc302685cf10fcdab800ed366fec
Viannez <58158004+Viannez@users.noreply.github.com>
2024-07-24 16:59:27 -0400

    Spring Boot Maven

---
+++ project1vz/src/main/java/com/skillstorm/project1vz/Project1vzApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.project1vz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1vzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1vzApplication.class, args);
+	}
+
+}
---
+++ project1vz/src/test/java/com/skillstorm/project1vz/Project1vzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.project1vz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1vzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
