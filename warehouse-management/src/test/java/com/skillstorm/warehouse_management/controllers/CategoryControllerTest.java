234bb4c3f9fe2e9f1376d75a362498bc37edf7fd
Viannez <58158004+Viannez@users.noreply.github.com>
2024-09-06 00:35:42 -0400

    try again

--- warehouse-management/src/test/java/com/skillstorm/warehouse_management/controllers/CategoryControllerTest.java
+++ warehouse-management/src/test/java/com/skillstorm/warehouse_management/controllers/CategoryControllerTest.java
@@ -27,7 +27,7 @@ public class CategoryControllerTest {
     @Mock
     private CategoryRepository categoryRepository;
 
-    //injects the Categorieservice mock object into the CategoryController
+    //injects the CategoryRepository mock object into the CategoryController
     @InjectMocks
     private CategoryController categoryController;
     private AutoCloseable closeable;
@@ -44,42 +44,53 @@ public class CategoryControllerTest {
         closeable.close();
     }
 
+    //find all categories
     @Test
     public void findAllCategoriesTest() {
-        //Given
+        //Given expected categories
         List<Category> expectedCategories = Arrays.asList(new Category(), new Category());
 
-        //When (stubbing)
+        //When (stubbing) findall method
         when(categoryRepository.findAll())
         
-        //Then
+        //Then return expected
         .thenReturn(expectedCategories);
 
+        //response from controller
         ResponseEntity<List<Category>> response = categoryController.findAll();
 
+        //check the response 
         Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
         Assert.assertEquals(response.getBody(), expectedCategories);
         Assert.assertEquals(response.getBody().size(), 2);
     }
-
+    
+    //find by category id
     @Test
     public void findCategoryByIdTest() {
+
+        //Given input and expected category
         Category inputCategory = new Category(1, "testname");
         Optional<Category> expectedCategory = Optional.of(inputCategory);
         
+        //When stubbing findbyid method
         when(categoryRepository.findById(inputCategory.getId()))
         .thenReturn(expectedCategory);
 
+        //response
         ResponseEntity<Category> response = categoryController.findById(inputCategory.getId());
 
+        //check response
         Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
     }
 
+    //test invalid fid by categoryid
     @Test
     public void findCategoryByIdInvalidTest() {
         Category inputCategory = new Category(1, "testname");
         Optional<Category> expectedCategory = Optional.of(inputCategory);
         
+        //
         when(categoryRepository.findById(1))
         .thenReturn(expectedCategory);
 
